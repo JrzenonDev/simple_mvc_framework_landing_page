70aaea6d577323c0ca7796e78b1e343e66f8ca88
JrzenonDev <jrobertoonb@gmail.com>
2016-02-26 00:06:50 -0300

    Refatoracao de nome de rota + Criacao de rota para edicao de usuario

--- app/Core/routes.php
+++ app/Core/routes.php
@@ -11,6 +11,7 @@ use Helpers\Hooks;
 Router::any('', 'Controllers\Home@index');
 Router::any('contact', 'Controllers\Contact@index');
 Router::any('painel_contato_usuario', 'Controllers\PainelContatoUsuario@ListUser');
+Router::any('edit_contato_usuario/(:num)', 'Controllers\PainelContatoUsuario@GetUser');
 Router::any('edit_contato_usuario/(:num)', 'Controllers\PainelContatoUsuario@EditUser');
 
 /** Module routes. */
